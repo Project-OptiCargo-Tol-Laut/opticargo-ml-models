28c7b0691bcc1d48e316e20d7b0981024965f2ee
unknown <satriahadiwiyana7@gmail.com>
2026-07-26 00:08:15 +0700

    chore: menambahkan kerangka file features.py untuk proses ekstraksi fitur ML

---
+++ training/scoring_model/features.py
@@ -0,0 +1,6 @@
+def extract_features(raw_data):
+    """
+    Placeholder: Fungsi ini nantinya akan mengubah data mentah 
+    menjadi fitur (vektor/matriks) yang siap digunakan untuk training model ML.
+    """
+    pass
